ebfb906983ddc299bdde9b9df5f293d04192b8b1
최학준 <>
2021-12-02 21:50:29 +0900

    Add left behind  files

---
+++ src/test/java/com/querydsl/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querydsl.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
