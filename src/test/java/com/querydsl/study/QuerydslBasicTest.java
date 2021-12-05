075983af9772e7cc828f5b2c4dde92057ae11fcb
최학준 <>
2021-12-05 23:52:32 +0900

    condition - search, andParam, result(fetch), sort

--- src/test/java/com/querydsl/study/QuerydslBasicTest.java
+++ src/test/java/com/querydsl/study/QuerydslBasicTest.java
@@ -1,10 +1,12 @@
 package com.querydsl.study;
 
+import com.querydsl.core.NonUniqueResultException;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.study.entity.Member;
 import com.querydsl.study.entity.QMember;
 import com.querydsl.study.entity.Team;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import java.util.List;
 
+import static com.querydsl.study.entity.QMember.*;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -60,15 +64,132 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl() {
-        QMember m = QMember.member;
 
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void searchForAndOrPriority() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10))
+                                .or(member.age.eq(10)))
+                        .fetchOne();
+
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10).or(member.age.eq(10)))
+                        .or(member.age.eq(10)))
+                .fetchOne();
+
+
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1")
+                        ,member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void resultFetch() {
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        assertThat(fetch instanceof List).isTrue();
+
+        //단 건
+        Assertions.assertThrows(NonUniqueResultException.class,
+                () -> queryFactory
+                    .selectFrom(QMember.member)
+                    .fetchOne()
+        );
+
+        // limit(1) 단 건 조회
+        Member firstMember = queryFactory
+                .selectFrom(QMember.member)
+                .fetchFirst();
+        assertThat(firstMember).isNotNull();
+
+
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        //토탈
+        System.out.println("results.getTotal() = " + results.getTotal());
+        //컨탠츠
+        System.out.println("results.getResults() = " + results.getResults());
+
+
+
+        //카운트 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
+        assertThat(count).isGreaterThanOrEqualTo(0);
+    }
+
+
+    /**
+     * 멤버 정렬 순서
+     * 1. 멤버 나이 내림차순
+     * 2. 멤버 이름 올림차순
+     * 3. 2에서 멤버 이름이 없으면 마지막에 출력 nulls last
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+
+        Member member1 = fetch.get(0);
+        Member member2 = fetch.get(fetch.size() - 1);
+        assertThat(member1.getUsername()).isEqualTo("member5");
+        assertThat(member2.getUsername()).isNull();
     }
 }
 
